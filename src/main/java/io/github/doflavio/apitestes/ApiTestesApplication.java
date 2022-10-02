590a48726673a72c9e4ff75d7ac797405f86fdf6
Flavio <doflavio@gmail.com>
2022-10-01 23:51:55 -0300

    Iniciando projeto

---
+++ src/main/java/io/github/doflavio/apitestes/ApiTestesApplication.java
@@ -0,0 +1,14 @@
+package io.github.doflavio.apitestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestesApplication.class, args);
+		System.out.println("teste3");
+	}
+
+}
